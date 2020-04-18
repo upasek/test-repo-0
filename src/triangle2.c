8ad341eabbe6642a1c55782570658276ac7cdca4
upasek <kvsupase@gmail.com>
2020-04-18 09:48:36 +0530

    adding another c code  Changes to be committed: 	new file:   src/triangle2.c

---
+++ src/triangle2.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+void main () {
+int i,j,n;
+printf ("Enter the value of n:");
+scanf ("%d",&n);
+
+for (i=n;i>=1;i--) 
+{
+for (j=1;j<=i;j++)
+{
+printf ("*");
+
+}
+
+printf ("\f");
+
+} 
+
+for (i=1;i<=n;i++)
+{
+
+for (j=1;j<=i;j++)
+{
+
+printf ("*");
+}
+printf ("\f");
+}
+
+
+printf ("\n");
+
+
+}
